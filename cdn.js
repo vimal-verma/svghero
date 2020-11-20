9cae539a2696ff34fdeaa72652fe627ab5fe8b65
vimalverma558 <kumarvimal558@gmail.com>
2020-11-20 14:56:49 +0530

    width height x and y added

--- cdn.js
+++ cdn.js
@@ -1,27 +1,30 @@
-const svghero = (id,title,text) =>
+const svghero = (id,title,text,width,height,x,y) =>
 {
+    let Uid = document.getElementById(id)
 
-    const x = document.getElementsByTagName("BODY")[0];
+    const Body = document.getElementsByTagName("BODY")[0];
     let svg_hero = document.createElement("div");
     svg_hero.classList.add('svg-hero')
-    x.appendChild(svg_hero)
+    Body.appendChild(svg_hero)
 
 
-    let svg = id.innerHTML;
+    let svg = Uid.innerHTML;
     // console.log(svg)
 
-    id.addEventListener('mouseover', () => {
-        id.style.scale = "1.004";
+    Uid.addEventListener('mouseover', () => {
+        Uid.style.scale = "1.004";
     })
-    id.addEventListener('mouseout', () => {
-        id.style.scale = "1";
+    Uid.addEventListener('mouseout', () => {
+        Uid.style.scale = "1";
     })
 
 
 
     let showsvg = `
-    <svg width="1152" height="700" viewBox="0 0 1152 700" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="${width}" height="${height}" viewBox="0 0 ${width} ${height}" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g transform="translate(${x},${y})">
         ${svg}
+        </g>
     </svg>
 
     `
@@ -40,7 +43,7 @@ const svghero = (id,title,text) =>
 
 
     
-    id.addEventListener('click', () => {
+    Uid.addEventListener('click', () => {
         svg_hero.innerHTML = htmlshow
 
         const canbut = document.getElementById('canbut')
