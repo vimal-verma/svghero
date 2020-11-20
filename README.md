3cfd0dd95b7694ce115eb82e27c7c4e7f0945873
vimalverma558 <kumarvimal558@gmail.com>
2020-11-20 19:44:51 +0530

    npm support added

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # svghero: Javascript package for hero effect in SVG
 ## Under Development
+
+
+https://cdn.jsdelivr.net/npm/svghero@0.1.0/cdn.js
--- index.js
+++ index.js
@@ -1,10 +1,54 @@
-module.exports.svghero =  (id,title,text) =>
+module.exports.svghero =  (id,title,text,width,height,x,y) =>
 {
+    let Uid = document.getElementById(id)
+
+    const Body = document.getElementsByTagName("BODY")[0];
+    let svg_hero = document.createElement("div");
+    svg_hero.classList.add('svg-hero')
+    Body.appendChild(svg_hero)
 
 
-    // Code
+    let svg = Uid.innerHTML;
+    // console.log(svg)
+
+    Uid.addEventListener('mouseover', () => {
+        Uid.style.scale = "1.004";
+    })
+    Uid.addEventListener('mouseout', () => {
+        Uid.style.scale = "1";
+    })
 
 
 
-    return svghero;
+    let showsvg = `
+    <svg width="${width}" height="${height}" viewBox="0 0 ${width} ${height}" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g transform="translate(${x},${y})">
+        ${svg}
+        </g>
+    </svg>
+
+    `
+
+    let htmlshow= `
+    <div class="showdiv">
+        <div class="title"><h1>${title}</h1></div>
+        <div id="canbut">X</div>
+        <div class="imgsec">
+            <div class="img">${showsvg}</div>
+            <div class="text">${text}</div>
+        </div>
+    </div>
+    `
+
+
+
+    
+    Uid.addEventListener('click', () => {
+        svg_hero.innerHTML = htmlshow
+
+        const canbut = document.getElementById('canbut')
+        canbut.addEventListener('click', () => {
+            svg_hero.innerHTML = null
+        })
+    })
 };
